ae7556f9c2f7bd4e15068cabbeceb16c92950705
Trever Faden <trever@users.noreply.github.com>
2015-05-20 12:56:31 -0400

    Add Optional *this* as last parameter to .then method for better use with prototyped functions

--- test.js
+++ test.js
@@ -14,15 +14,41 @@ function msg(msgd, cb){
 };
 
 
-xy.emit('magic')
-	.then(fs.readFile)
-	.then(function(file){
-		console.log(file);
-	})
-	.catch(function(e){
-		throw(e);
-		console.log('e', e);
-	});
+// xy.emit('magic')
+// 	.then(fs.readFile)
+// 	.then(function(file){
+// 		console.log(file);
+// 	})
+// 	.catch(function(e){
+// 		throw(e);
+// 		console.log('e', e);
+// 	});
+var q = function(){
+	var oode = 'magic'
+	this.invite = {
+		magic:true
+	};
+	return this;
+};
+
+q.prototype.yes = function(text, cb){
+	console.log(this);
+	console.log('hello');
+	cb(null, 'yes');
+}
+
+q.prototype.marvingaye = function(text, cb){
+	console.log(text);
+	console.log(this.invite);
+	cb(null, 'this is logged at three');
+};
+
+q.prototype.three = function(text){
+	console.log(text);
+};
+
+var m = new q;
+m.yes.emit('magic').then(m.marvingaye, m).then(m.three);
 
 // xy('magic', function(e,r){
 // 	console.log(r);
